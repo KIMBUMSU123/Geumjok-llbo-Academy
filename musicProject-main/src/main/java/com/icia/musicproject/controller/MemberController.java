4fb98859562a099324e7004004949375ce1b41d3
akrrjffl <souij02060@naver.com>
2023-11-01 16:06:58 +0900

    회원가입 진행중 (중복체크 기능 미완성) 로그인 페이지 작업중

--- musicProject-main/src/main/java/com/icia/musicproject/controller/MemberController.java
+++ musicProject-main/src/main/java/com/icia/musicproject/controller/MemberController.java
@@ -3,11 +3,11 @@ package com.icia.musicproject.controller;
 import com.icia.musicproject.DTO.MemberDTO;
 import com.icia.musicproject.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
@@ -29,19 +29,48 @@ public class MemberController {
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login(@RequestParam(value = "redirectURI", defaultValue = "/member/login") String redirectURI,
+                        Model model) {
+        model.addAttribute("redirectURI", redirectURI);
         return "/member/login";
     }
-    @PostMapping("/login")
-    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
-        boolean loginResult = memberService.login(memberDTO);
-        if (loginResult) {
-            session.setAttribute("loginEmail", memberDTO.getMemberEmail());
 
-            return "/index";
+    @PostMapping("/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session,
+                        @RequestParam("redirectURI") String redirectURI) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            session.setAttribute("loginNickname", memberDTO.getMemberNickname());
+//            session.setAttribute("loginEmail", memberDTO.getMemberEmail());
+            session.setAttribute("loginId", loginResult.getId());
+            return "redirect:" + redirectURI;
         } else {
             return "/member/login";
         }
     }
 
+    @GetMapping("/myPage")
+    public String myPage() {
+        return "/member/main";
+    }
+    // 이메일 중복체크 기능 미완성
+    @PostMapping("/dup-check")
+    public ResponseEntity emailCheck(@RequestBody MemberDTO memberDTO) {
+        boolean result = memberService.emailCheck(memberDTO.getMemberEmail());
+        if (result) {
+            return new ResponseEntity<>("사용가능", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("사용불가능", HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping("/axios/{id}")
+    public ResponseEntity detailAxios(@PathVariable("id") Long id) {
+        try {
+            MemberDTO memberDTO = memberService.findById(id);
+            return new ResponseEntity<>(memberDTO, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- musicProject-main/src/main/java/com/icia/musicproject/service/MemberService.java
+++ musicProject-main/src/main/java/com/icia/musicproject/service/MemberService.java
@@ -6,6 +6,9 @@ import com.icia.musicproject.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 @RequiredArgsConstructor
 @Service
@@ -14,10 +17,12 @@ public class MemberService {
 
     public Long save(MemberDTO memberDTO) {
         MemberEntity memberEntity = MemberEntity.toSaveEntity(memberDTO);
+        System.out.println("memberEntity = " + memberEntity);
+        System.out.println("memberDTO = " + memberDTO);
         return memberRepository.save(memberEntity).getId();
     }
 
-    public boolean login(MemberDTO memberDTO) {
+    public MemberDTO login(MemberDTO memberDTO) {
 /*
 DB에서 로그인하는 사용자의 이메일로 조회한 결과를 가져와서
 비밀번호가 일치하는지 비교한 뒤 로그인 성공 여부를 판단
@@ -32,10 +37,33 @@ findById()
 //        MemberEntity memberEntity = memberRepository.findByMemberEmail(memberDTO.getMemberEmail())
 //                                                    .orElseThrow(() -> new NoSuchElementException());
 // 2. email, password 둘다 만족하는 조회결과가 있다면 로그인성공, 없다면 로그인실패
-        Optional<MemberEntity> optionalMemberEntity =
+        Optional<MemberEntity> memberEntity =
                 memberRepository.findByMemberEmailAndMemberPw(memberDTO.getMemberEmail(), memberDTO.getMemberPw());
-        if (optionalMemberEntity.isPresent()) {
+        if (memberEntity.isPresent()) {
             //            MemberEntity memberEntity = optionalMemberEntity.get();
+            return MemberDTO.toDTO(memberEntity.get());
+        } else {
+            return null;
+        }
+    }
+
+    public List<MemberDTO> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        for (MemberEntity memberEntity: memberEntityList) {
+            memberDTOList.add(MemberDTO.toDTO(memberEntity));
+        }
+        return memberDTOList;
+    }
+
+    public MemberDTO findById(Long id) {
+        MemberEntity memberEntity = memberRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+        return MemberDTO.toDTO(memberEntity);
+    }
+
+    public boolean emailCheck(String memberEmail) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(memberEmail);
+        if (optionalMemberEntity.isEmpty()) {
             return true;
         } else {
             return false;
